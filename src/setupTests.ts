04faed19880288b1d9d14d0d28e48d0606d2164a
slikts <dabas@untu.ms>
2018-12-19 13:35:18 +0200

    format

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,11 +1,10 @@
 /* tslint:disable no-var-requires */
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import {GlobalWithFetchMock} from "jest-fetch-mock";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { GlobalWithFetchMock } from "jest-fetch-mock";
 
 const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
-customGlobal.fetch = require('jest-fetch-mock');
+customGlobal.fetch = require("jest-fetch-mock");
 customGlobal.fetchMock = customGlobal.fetch;
-
 
 configure({ adapter: new Adapter() });
