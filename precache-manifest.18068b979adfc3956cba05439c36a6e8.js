5836504687b4b2cd7173cebce860af7032ac9ffe
slikts <dabas@untu.ms>
2018-12-19 13:32:35 +0200

    Updates

---
+++ precache-manifest.18068b979adfc3956cba05439c36a6e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b68f17c61779a88afa8a",
+    "url": "/static/js/main.b68f17c6.chunk.js"
+  },
+  {
+    "revision": "cf7ede3fbcab3c41014b",
+    "url": "/static/js/1.cf7ede3f.chunk.js"
+  },
+  {
+    "revision": "b68f17c61779a88afa8a",
+    "url": "/static/css/main.e6b8d6a1.chunk.css"
+  },
+  {
+    "revision": "f44b5f9b94ce9ac578e76745c337d1ce",
+    "url": "/index.html"
+  }
+];
